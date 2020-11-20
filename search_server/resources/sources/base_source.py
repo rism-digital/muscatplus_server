f22a29981e556913f384343cdaa5f0dc50ebeefd
Andrew Hankinson <andrew.hankinson@gmail.com>
2020-11-20 15:00:51 +0100

    Update base source response
    More suitable for embedded source representations

--- search_server/resources/sources/base_source.py
+++ search_server/resources/sources/base_source.py
@@ -5,8 +5,7 @@ import serpy
 
 from search_server.helpers.display_fields import get_display_fields
 from search_server.helpers.fields import StaticField
-from search_server.helpers.identifiers import ID_SUB, get_identifier
-from search_server.helpers.ld_context import RISM_JSONLD_CONTEXT
+from search_server.helpers.identifiers import ID_SUB, get_identifier, RISM_JSONLD_CONTEXT, get_jsonld_context
 from search_server.helpers.serializers import ContextDictSerializer
 from search_server.helpers.solr_connection import SolrResult
 
@@ -28,17 +27,14 @@ class BaseSource(ContextDictSerializer):
         label="type",
         value="rism:Source"
     )
-    display = serpy.MethodField(
-        label="display"
-    )
-
+    title = serpy.MethodField()
     part_of = serpy.MethodField(
         label="partOf"
     )
 
     def get_ctx(self, obj: Dict) -> Dict:
         direct_request: Optional[bool] = self.context.get("direct_request")
-        return RISM_JSONLD_CONTEXT if direct_request else None
+        return get_jsonld_context(self.context.get("request")) if direct_request else None
 
     def get_sid(self, obj: Dict) -> str:
         req = self.context.get('request')
@@ -46,11 +42,12 @@ class BaseSource(ContextDictSerializer):
 
         return get_identifier(req, "source", source_id=source_id)
 
-    def get_display(self, obj: SolrResult) -> List[Dict]:
+    def get_title(self, obj: SolrResult) -> List[Dict]:
         req = self.context.get("request")
         transl: Dict = req.app.translations
+        field_config: Dict = {"title_s": "records.title"}
 
-        return get_display_fields(obj, transl)
+        return get_display_fields(obj, transl, field_config)
 
     def get_part_of(self, obj: Dict) -> Optional[List]:
         # Do not show 'partOf' if the result is embedded in the source it is part of.
