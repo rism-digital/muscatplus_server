382edc1c22bde75d51ac1ef6b18ac5eee018fd5c
Andrew Hankinson <andrew.hankinson@rism.digital>
2021-02-04 16:06:31 +0100

    Add facet value label configurations

--- search_server/resources/search/facets.py
+++ search_server/resources/search/facets.py
@@ -1,5 +1,6 @@
 from typing import Optional, List, Dict, Tuple
 import logging
+import urllib.parse
 
 import pysolr
 import serpy
@@ -27,15 +28,28 @@ class FacetList(ContextDictSerializer):
         facet_config = cfg['search']["facet_fields"]
         # Generate a lookup table for the alias / display so that we don't have to do this in the loop below.
         facet_display_config: Dict = {f"{cfg['alias']}": f"{cfg['display_name']}" for cfg in facet_config.values()}
+        facet_value_displayname_map: Dict = {f"{cfg['alias']}": cfg['display_value_map'] for cfg in facet_config.values() if cfg.get('display_value_map')}
 
         facets: List[Dict] = []
 
         for alias, res in facet_result.items():
-            # Ignore the 'count' field
+            # Ignore the 'count' field in the solr result
             if alias == "count":
                 continue
 
-            items: List = [{"value": f"{bucket['val']}", "count": f"{bucket['count']}"} for bucket in res["buckets"]]
+            items: List = []
+            for bucket in res["buckets"]:
+                displayName: str
+                if alias in facet_value_displayname_map and (d := facet_value_displayname_map[alias].get(str(bucket['val']))):
+                    display_name = d  # ignore warning
+                else:
+                    display_name = bucket['val']
+
+                items.append({
+                    "value": urllib.parse.quote_plus(str(bucket['val'])),
+                    "label": {"none": [display_name]},
+                    "count": bucket['count']
+                })
 
             # If we don't have a list of values, don't show the facet.
             if not items:
@@ -43,7 +57,7 @@ class FacetList(ContextDictSerializer):
 
             f = {
                 "alias": alias,
-                "displayName": facet_display_config[alias],
+                "label": {"none": [facet_display_config[alias]]},
                 "items": items
             }
             facets.append(f)
