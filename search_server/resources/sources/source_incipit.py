00b27c6b05153747550aca7652bbed75ed90c371
Andrew Hankinson <andrew.hankinson@rism.digital>
2021-02-23 13:04:18 +0100

    Improve incipit response
    Now responds with pre-rendered SVG and midi

--- search_server/resources/sources/source_incipit.py
+++ search_server/resources/sources/source_incipit.py
@@ -4,18 +4,66 @@ from typing import Dict, Optional, List
 import pysolr
 import serpy
 
+import ujson
+import verovio
+
+from search_server.helpers.display_fields import get_display_fields
 from search_server.helpers.fields import StaticField
-from search_server.helpers.identifiers import ID_SUB, get_identifier, RISM_JSONLD_CONTEXT, get_jsonld_context, \
+from search_server.helpers.identifiers import (
+    ID_SUB,
+    get_identifier,
+    get_jsonld_context,
     JSONLDContext
+)
 from search_server.helpers.serializers import ContextDictSerializer
 from search_server.helpers.solr_connection import SolrConnection, SolrResult, SolrManager
 
+vrv_tk = verovio.toolkit()
+vrv_tk.setInputFrom(verovio.PAE)
+vrv_tk.setOptions(ujson.dumps({
+    "footer": 'none',
+    "header": 'none',
+    "pageMarginTop": 10,
+    "pageMarginBottom": 10,
+    "pageMarginLeft": 10,
+    "pageMarginRight": 10,
+    "pageWidth": 1024 / 0.4,
+    "spacingStaff": 1,
+    "scale": 40,
+    "adjustPageHeight": 1,
+    "svgHtml5": "true",
+    "svgFormatRaw": "true"
+}))
 
-def handle_incipits_list_request(req, source_id: str) -> Optional[Dict]:
-    pass
+
+def _incipit_to_pae(obj: SolrResult) -> str:
+    """
+    This function assumes that all the data is complete in the
+    Solr result; any checks for whether the data needed to return
+    the PAE code is present should be done before calling this function.
+    :param obj: A Solr result object for an incipit.
+    :return: A string formatted as Plaine and Easie code
+    """
+    clef = obj.get("clef_s")
+    timesig = obj.get("timesig_s")
+    key_or_mode = obj.get("key_mode_s")
+    keysig = obj.get("key_s")
+    incip = obj.get("music_incipit_s")
+
+    pae_code: str = f"""
+            @start:pae-file
+            @clef:{clef}
+            @keysig:{keysig}
+            @key:{key_or_mode}
+            @timesig:{timesig}
+            @data:{incip}
+            @end:pae-file
+            """
+
+    return pae_code
 
 
-def handle_incipit_request(req, source_id: str, work_num: str) -> Optional[Dict]:
+def _fetch_incipit(source_id: str, work_num: str) -> Optional[SolrResult]:
     fq: List = ["type:source_incipit",
                 f"source_id:source_{source_id}",
                 f"work_num_s:{work_num}"]
@@ -25,7 +73,19 @@ def handle_incipit_request(req, source_id: str, work_num: str) -> Optional[Dict]
     if record.hits == 0:
         return None
 
-    incipit_record = record.docs[0]
+    return record.docs[0]
+
+
+def handle_incipits_list_request(req, source_id: str) -> Optional[Dict]:
+    pass
+
+
+def handle_incipit_request(req, source_id: str, work_num: str) -> Optional[Dict]:
+    incipit_record: Optional[SolrResult] = _fetch_incipit(source_id, work_num)
+
+    if not incipit_record:
+        return None
+
     incipit = SourceIncipit(incipit_record, context={"request": req,
                                                      "direct_request": True})
 
@@ -85,6 +145,9 @@ class SourceIncipit(ContextDictSerializer):
     incip_id = serpy.MethodField(
         label="id"
     )
+    label = serpy.MethodField()
+    summary = serpy.MethodField()
+
     itype = StaticField(
         label="type",
         value="rism:Incipit"
@@ -93,21 +156,11 @@ class SourceIncipit(ContextDictSerializer):
         attr="title_s",
         required=False
     )
-    music_incipit = serpy.StrField(
-        label="musicIncipit",
-        attr="music_incipit_s",
-        required=False
+    text_incipit = serpy.MethodField(
+        label="textIncipit"
     )
-    text_incipit = serpy.StrField(
-        label="textIncipit",
-        attr="text_incipit_s",
-        required=False
-    )
-    work_number = serpy.StrField(
-        label="workNumber",
-        attr="work_num_s",
-        required=False
-    )
+
+    rendered = serpy.MethodField()
 
     def get_ctx(self, obj: Dict) -> Optional[Dict]:
         direct_request: bool = self.context.get("direct_request")
@@ -117,6 +170,57 @@ class SourceIncipit(ContextDictSerializer):
         req = self.context.get("request")
 
         source_id: str = re.sub(ID_SUB, "", obj.get("source_id"))
-        incipit_id: str = f"{obj.get('work_num_s')}"
+        work_num: str = f"{obj.get('work_num_s')}"
 
-        return get_identifier(req, "incipit", source_id=source_id, incipit_id=incipit_id)
+        return get_identifier(req, "incipit", source_id=source_id, work_num=work_num)
+
+    def get_label(self, obj: SolrResult) -> Optional[Dict]:
+        title: Optional[str] = obj.get("title_s")
+        t_incipit: Optional[str] = obj.get("text_incipit_s")
+
+        if not title and not t_incipit:
+            return {
+                "none": ["[No title]"]
+            }
+
+        if title:
+            label = title
+        else:
+            label = f"[{t_incipit}]"
+
+        return {
+            "none": [f"{label}"]
+        }
+
+    def get_summary(self, obj: SolrResult) -> List[Dict]:
+        req = self.context.get("request")
+        transl: Dict = req.app.translations
+
+        return get_display_fields(obj, transl)
+
+    def get_text_incipit(self, obj: SolrResult) -> Optional[Dict]:
+        if not obj.get("text_incipit_s"):
+            return None
+
+        return {
+            "none": [f"{obj.get('text_incipit_s')}"]
+        }
+
+    def get_rendered(self, obj: SolrResult) -> Optional[List]:
+        if not obj.get("music_incipit_s"):
+            return None
+
+        pae_code: str = _incipit_to_pae(obj)
+
+        vrv_tk.loadData(pae_code)
+        svg: str = vrv_tk.renderToSVG()
+        mid: str = vrv_tk.renderToMIDI()
+        b64midi = f"data:audio/midi;base64,{mid}"
+
+        return [{
+            "format": "image/svg+xml",
+            "data": svg
+        }, {
+            "format": "audio/midi",
+            "data": b64midi
+        }]
