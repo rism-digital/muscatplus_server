01a7e86dd787b0cfa22d24e466b7ff28b204eedc
Andrew Hankinson <andrew.hankinson@rism.digital>
2021-03-08 14:32:32 +0100

    Work on Material Group relationships

--- search_server/resources/sources/source_materialgroup.py
+++ search_server/resources/sources/source_materialgroup.py
@@ -4,10 +4,22 @@ from typing import Dict, List, Optional
 import pysolr
 import serpy
 
+from search_server.helpers.display_fields import get_display_fields, LabelConfig
 from search_server.helpers.fields import StaticField
-from search_server.helpers.identifiers import ID_SUB, get_identifier
+from search_server.helpers.identifiers import (
+    ID_SUB,
+    get_identifier,
+    JSONLDContext,
+    get_jsonld_context,
+    RELATIONSHIP_LABELS,
+    QUALIFIER_LABELS
+)
+
 from search_server.helpers.serializers import ContextDictSerializer
-from search_server.helpers.solr_connection import SolrConnection, SolrManager
+from search_server.helpers.solr_connection import SolrConnection, SolrManager, SolrResult
+
+import logging
+log = logging.getLogger(__name__)
 
 
 def handle_materialgroups_list_request(req, source_id: str) -> Optional[Dict]:
@@ -35,25 +47,26 @@ class SourceMaterialGroupList(ContextDictSerializer):
     label = serpy.MethodField()
     items = serpy.MethodField()
 
-    def get_mid(self, obj: Dict) -> str:
+    def get_mid(self, obj: SolrResult) -> str:
         req = self.context.get("request")
         source_id: str = re.sub(ID_SUB, "", obj.get("source_id"))
 
         return get_identifier(req, "materialgroups_list", source_id=source_id)
 
-    def get_label(self, obj: Dict) -> Dict:
+    def get_label(self, obj: SolrResult) -> Dict:
         req = self.context.get("request")
         transl: Dict = req.app.translations
 
         return transl.get("records.material_description")
 
-    def get_items(self, obj: Dict) -> Optional[List]:
+    def get_items(self, obj: SolrResult) -> Optional[List]:
         conn = SolrManager(SolrConnection)
         fq: List = ["type:source_materialgroup",
                     f"source_id:{obj.get('source_id')}"]
+        fl: str = "*,people_json:[json],institutions_json:[json],external_links_json:[json]"
         sort: str = "group_num_s asc"
 
-        conn.search("*:*", fq=fq, sort=sort)
+        conn.search("*:*", fq=fq, fl=fl, sort=sort)
 
         if conn.hits == 0:
             return None
@@ -76,11 +89,14 @@ class SourceMaterialGroup(ContextDictSerializer):
         value="rism:MaterialGroup"
     )
     label = serpy.MethodField()
+    summary = serpy.MethodField()
+    related = serpy.MethodField()
 
-    def get_ctx(self, obj: Dict) -> Dict:
-        pass
+    def get_ctx(self, obj: SolrResult) -> Optional[JSONLDContext]:
+        direct_request: Optional[bool] = self.context.get("direct_request")
+        return get_jsonld_context(self.context.get("request")) if direct_request else None
 
-    def get_mid(self, obj: Dict) -> str:
+    def get_mid(self, obj: SolrResult) -> str:
         req = self.context.get("request")
 
         source_id: str = re.sub(ID_SUB, "", obj.get("source_id"))
@@ -88,5 +104,103 @@ class SourceMaterialGroup(ContextDictSerializer):
 
         return get_identifier(req, "materialgroup", source_id=source_id, materialgroup_id=materialgroup_id)
 
-    def get_label(self, obj: Dict) -> List[Dict]:
-        pass
+    def get_label(self, obj: SolrResult) -> Optional[Dict]:
+        group_num: Optional[str] = obj.get("group_num_s")
+        if not group_num:
+            return None
+
+        return {"none": [f"Group {group_num}"]}
+
+    def get_related(self, obj: SolrResult) -> Optional[Dict]:
+        people_json = obj.get("people_json")
+        institutions_json = obj.get("institutions_json")
+        req = self.context.get("request")
+
+        # These will always return a list, even if the data passed in is empty.
+        people_items: List = _relationshiplist_from_json(people_json, "person", req)
+        institution_items: List = _relationshiplist_from_json(institutions_json, "institution", req)
+
+        all_items = people_items + institution_items
+
+        if not all_items:
+            return None
+
+        transl: Dict = req.app.translations
+
+        return {
+            "label": transl.get("records.people_institutions"),
+            "type": "rism:RelationshipList",
+            "items": all_items
+        }
+
+    def get_summary(self, obj: SolrResult) -> List[Dict]:
+        req = self.context.get("request")
+        transl: Dict = req.app.translations
+
+        fields: LabelConfig = {
+            "source_type_sm": ("records.source_type", None),
+            "date_statements_sm": ("records.date", None),
+            "physical_extent_sm": ("records.extent", None),
+            "parts_held_sm": ("records.parts_held", None),
+            "parts_extent_sm": ("records.parts_held_extent", None),
+            "plate_numbers_sm": ("records.plate_number", None),
+            "general_notes_sm": ("records.general_note", None)
+        }
+
+        return get_display_fields(obj, transl, field_config=fields)
+
+
+def _relationshiplist_from_json(fielddata: List, reltype: str, req) -> List:
+    if not fielddata:
+        return []
+
+    transl: Dict = req.app.translations
+
+    items: List = []
+    for rel in fielddata:
+        source_rel: Dict = {
+            "type": "rism:SourceRelationship"
+        }
+        name: Optional[str] = rel.get("name")
+        role: Optional[str] = rel.get("role")
+        qualifier: Optional[str] = rel.get("qualifier")
+
+        if role:
+            r_translation_key: str = RELATIONSHIP_LABELS.get(role)
+            source_rel.update({
+                "role": {
+                    "type": f"relators:{role}",
+                    "label": transl.get(r_translation_key)
+                }
+            })
+
+        if qualifier:
+            q_translation_key: str = QUALIFIER_LABELS.get(qualifier)
+            source_rel.update({
+                "qualifier": {
+                    "type": f"rismdata:{qualifier}",
+                    "label": transl.get(q_translation_key)
+                }
+            })
+
+        if name:
+            rel_id = rel.get("id")
+            rel_num: str = re.sub(ID_SUB, "", rel_id)
+
+            if reltype == "person":
+                identifier = get_identifier(req, "person", person_id=rel_num)
+                objtype = "rism:Person"
+            else:
+                identifier = get_identifier(req, "institution", institution_id=rel_num)
+                objtype = "rism:Institution"
+
+            source_rel.update({
+                "relatedTo": {
+                    "id": identifier,
+                    "type": objtype,
+                    "label": {"none": [name]}
+                }
+            })
+        items.append(source_rel)
+
+    return items
