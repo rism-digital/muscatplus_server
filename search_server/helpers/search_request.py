bee168f1122d7aa27697080a85c3fc438f0f8f7e
Andrew Hankinson <andrew.hankinson@rism.digital>
2021-04-06 16:33:01 +0200

    New: Rework search request compiling
    Adds a new search 'mode' which lets us switch the type of results we're looking at.
    
    Refs rism-digital/muscatplus_ui#19

--- search_server/helpers/search_request.py
+++ search_server/helpers/search_request.py
@@ -10,6 +10,25 @@ import ujson
 log = logging.getLogger(__name__)
 
 
+# Some helper functions to work with the field and filter configurations.
+def filters_for_mode(cfg: Dict, mode: str) -> Dict:
+    filter_configuration: Dict = cfg["search"]["filters"]
+    filts: Optional[Dict] = filter_configuration.get(mode)
+    return filts or {}
+
+
+def field_alias_map(filters_config: Dict) -> Dict:
+    return {f"{cnf['alias']}": f"{solr_f}" for solr_f, cnf in filters_config.items()}
+
+
+def display_name_alias_map(filters_config: Dict) -> Dict:
+    return {f"{cfg['alias']}": f"{cfg['display_name']}" for cfg in filters_config.values()}
+
+
+def display_value_alias_map(filters_config: Dict) -> Dict:
+    return {f"{cfg['alias']}": cfg['display_value_map'] for cfg in filters_config.values() if cfg.get('display_value_map')}
+
+
 class SearchRequest:
     """
     Takes a number of parameters passed in from a search request and compiles them to produce a set of
@@ -26,29 +45,77 @@ class SearchRequest:
     def __init__(self, req, sort: Optional[str] = None):
         self._req = req
         self._app_config = req.app.config
+
+        # Validate the incoming request to see if we can parse it.
+        # This will raise an exception if there is anything wrong.
+        self._validate_incoming_request()
+
+        # Set up some public properties
         self.filters: List = []
+        self.sorts: List = []
+
         # If there is no q parameter it will return all results
         self._requested_query: str = req.args.get("q", "*:*")
+        self._requested_filters: List = req.args.getlist("fq", [])
+        self._requested_mode: str = req.args.get("mode", "everything")
         self._page: Optional[str] = req.args.get("page", None)
         self._return_rows: Optional[str] = req.args.get("rows", None)
-        self.sorts: List = []
-        self._alias_map = {f"{cnf['alias']}": f"{solr_f}" for solr_f, cnf in self._app_config["search"]["facet_fields"].items()}
 
-    def _get_requested_filters(self) -> List:
-        fqs: List = self._req.args.getlist("fq")
-        if not fqs:
-            # Return a list of the default filters
-            return ["type:source OR type:person OR type:institution OR type:place OR type:source_incipit"]
+        self._filters_for_mode: Dict = filters_for_mode(self._app_config, self._requested_mode)
+        self._alias_map: Dict = field_alias_map(self._filters_for_mode)
 
-        requested_filters: List = []
+    def _validate_incoming_request(self) -> None:
+        """
+        Raises an InvalidQueryException with specific responses for different error conditions.
+        Doing this once means we can assume that if it passes, we don't have to raise this exception
+        elsewhere in the class.
 
-        for filt in fqs:
-            # split the incoming filters
+        :return: None
+        """
+        valid_q_param: List = self._req.args.getlist("q", [])
+        if len(valid_q_param) > 1:
+            raise InvalidQueryException("Only one query parameter can be supplied.")
+
+        requested_modes: List = self._req.args.getlist("mode", [])
+        if len(requested_modes) > 1:
+            raise InvalidQueryException("Only one mode parameter can be supplied.")
+
+        modes: Dict = self._app_config["search"]["modes"]
+        if len(requested_modes) == 1:
+            requested_mode_config: Optional[Dict] = modes.get(requested_modes[0])
+
+            # if the requested mode does not match anything configured, raise an exception
+            if not requested_mode_config:
+                raise InvalidQueryException("Invalid value for the requested mode.")
+
+        try:
+            _ = parse_page_number(self._req.args.get("page", None))
+        except PaginationParseException as e:
+            raise InvalidQueryException(e)
+
+        try:
+            _ = parse_row_number(self._req, self._req.args.get("rows", None))
+        except PaginationParseException as e:
+            raise InvalidQueryException(e)
+
+    def _modes_to_filter(self) -> str:
+        """
+        Turns the incoming 'mode' request to a string suitable for use in a Solr `fq` query.
+
+        :return: A string with the appropriate "OR" statements applied.
+        """
+        modes: Dict = self._app_config["search"]["modes"]
+        mode_cfg: Dict = modes.get(self._requested_mode)
+        record_types: List = mode_cfg["record_types"]
+        solr_types_field: List = [f"type:{v}" for v in record_types]
+
+        return " OR ".join(solr_types_field)
+
+    def _compile_filters(self) -> List:
+        filter_statements: List = []
+
+        for filt in self._requested_filters:
             field, raw_value = filt.split(":")
-            # Map them to the actual solr fields
-            # Ignore any parameters not explicitly configured
-            if field not in self._alias_map:
-                continue
 
             # do some processing and normalization on the value. First ensure we have a non-entity string.
             # This should convert the URL-encoded parameters back to 'normal' characters
@@ -58,21 +125,22 @@ class SearchRequest:
             value: str = unencoded_value.replace("\"", "").replace("'", "")
 
             # Finally, ensure that we *always* pass it to Solr as a quoted value. (The single quotes here will
-            # get converted to double-quotes by the internals of the API).
-            new_val = f"{self._alias_map[field]}:\"{value}\""
-            requested_filters.append(new_val)
+            # get converted to double-quotes by the internals of the pysolr API). We use the previously-constructed
+            # alias map to map the public field name to the private solr field.
+            new_val: str = f"{self._alias_map[field]}:\"{value}\""
+            filter_statements.append(new_val)
 
-        return requested_filters
+        return filter_statements
 
     def _get_facets(self) -> str:
-        # This uses the alias feature for facets to map our 'public' name for the facet
-        # to the actual solr field name being used. This means that in the output of this
-        # (that is, when the search request is returned) the facets will be available by
-        # the alias, not by the solr field name as would be normally expected.
-        facet_cfg: Dict = self._app_config["search"]["facet_fields"]
+        # if there are no filter, return an empty string. This will cause Solr to not emit any facets through
+        # the JSON Facet API
+        if not self._filters_for_mode:
+            return ""
+
         json_facets: Dict = {}
 
-        for solr_field, field_cfg in facet_cfg.items():
+        for solr_field, field_cfg in self._filters_for_mode.items():
             field_alias = field_cfg["alias"]
 
             json_facets[field_alias] = {
@@ -87,32 +155,69 @@ class SearchRequest:
         return ujson.dumps(json_facets)
 
     def compile(self) -> Dict:
-        filters: List = self.filters + self._get_requested_filters()
+        mode_filter: str = self._modes_to_filter()
+        self.filters.append(mode_filter)
 
-        if self._req.args.get("q"):
-            sorts = ["score desc"]
-        else:
-            sorts = ["main_title_ans asc"]
+        requested_filters: List = self._compile_filters()
+        self.filters += requested_filters
 
-        # If page is set, try to parse out the page number from the
-        # value. If it's not a number, flag the request as invalid.
-        try:
-            page_num: int = parse_page_number(self._page)
-        except PaginationParseException:
-            raise InvalidQueryException("Invalid value for page. If provided, it must be a whole number greater than 1.")
-
-        try:
-            return_rows: int = parse_row_number(self._req, self._return_rows)
-        except PaginationParseException:
-            raise InvalidQueryException("Invalid value for rows.")
-
+        # These have already been checked in the validation, so they shouldn't raise an exception here.
+        page_num: int = parse_page_number(self._page)
+        return_rows: int = parse_row_number(self._req, self._return_rows)
         start_row: int = 0 if page_num == 1 else ((page_num - 1) * return_rows)
 
         return {
             "q": [self._requested_query],
-            "fq": filters,
+            "fq": self.filters,
             "start": start_row,
             "rows": return_rows,
-            "sort": ", ".join(sorts),
+            # "sort": ", ".join(sorts),
             "json.facet": self._get_facets()
         }
+
+    # def _get_requested_filters(self) -> List:
+    #     fqs: List = self._req.args.getlist("fq")
+    #     requested_filters: List = []
+    #
+    #     for filt in fqs:
+    #         # split the incoming filters
+    #         field, raw_value = filt.split(":")
+    #
+    #         # do some processing and normalization on the value. First ensure we have a non-entity string.
+    #         # This should convert the URL-encoded parameters back to 'normal' characters
+    #         unencoded_value: str = urllib.parse.unquote_plus(raw_value)
+    #
+    #         # Then remove any quotes (single or double)
+    #         value: str = unencoded_value.replace("\"", "").replace("'", "")
+    #
+    #     return requested_filters
+
+    # def _get_facets(self) -> str:
+    #     # This uses the alias feature for facets to map our 'public' name for the facet
+    #     # to the actual solr field name being used. This means that in the output of this
+    #     # (that is, when the search request is returned) the facets will be available by
+    #     # the alias, not by the solr field name as would be normally expected.
+    #     facet_cfg: Dict = self._app_config["search"]["facet_fields"]
+    #     json_facets: Dict = {}
+    #
+    #     for solr_field, field_cfg in facet_cfg.items():
+    #         field_alias = field_cfg["alias"]
+    #
+    #         json_facets[field_alias] = {
+    #             "type": "terms",
+    #             "field": solr_field,
+    #             "sort": f"{field_cfg['sort']}",
+    #             "limit": 50
+    #         }
+    #
+    #     # Serialize the JSON to a string so that it can go over the Solr API.
+    #     # PySolr will transparently 'do the right thing' with the request.
+    #     return ujson.dumps(json_facets)
+
+    # filters: List = self.filters + self._get_requested_filters()
+
+    # if self._req.args.get("q"):
+    #     sorts = ["score desc"]
+    # else:
+    #     sorts = ["main_title_ans asc"]
+
