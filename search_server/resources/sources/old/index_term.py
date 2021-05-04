24998b85cd780fb99fc74c7fb6d9069188b279d9
Andrew Hankinson <andrew.hankinson@rism.digital>
2021-05-04 16:01:09 +0200

    Clean up old code

--- search_server/resources/sources/old/index_term.py
+++
@@ -1,9 +0,0 @@
-from search_server.helpers.serializers import JSONLDContextDictSerializer
-from search_server.helpers.solr_connection import SolrResult
-
-
-class IndexTermsList(JSONLDContextDictSerializer):
-    pass
-
-    def items(self, obj: SolrResult):
-        pass
