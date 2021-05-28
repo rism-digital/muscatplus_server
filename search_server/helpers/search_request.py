3a78ad49fbb6ed06203ad85e8f004bb040ff4d6d
Andrew Hankinson <andrew.hankinson@rism.digital>
2021-05-28 10:20:01 +0200

    Improvements to the mode selector

--- search_server/helpers/search_request.py
+++ search_server/helpers/search_request.py
@@ -12,20 +12,47 @@ log = logging.getLogger(__name__)
 
 # Some helper functions to work with the field and filter configurations.
 def filters_for_mode(cfg: Dict, mode: str) -> Dict:
+    """
+    Returns a set of facets to apply when a particular result mode is chosen.
+    :param cfg: The application configuration
+    :param mode: The selected mode
+    :return: The list of filter fields from the configuration for a given mode, or an empty dictionary if no filters
+        are configured.
+    """
     filter_configuration: Dict = cfg["search"]["filters"]
     filts: Optional[Dict] = filter_configuration.get(mode)
     return filts or {}
 
 
 def field_alias_map(filters_config: Dict) -> Dict:
+    """
+    A dictionary mapping the configured alias to the Solr field. Used for referencing the solr field using the
+    alias.
+    :param filters_config: The application configuration
+    :return: A dictionary of values mapping the alias to the solr field.
+    """
     return {f"{cnf['alias']}": f"{solr_f}" for solr_f, cnf in filters_config.items()}
 
 
 def display_name_alias_map(filters_config: Dict) -> Dict:
+    """
+    A dictionary mapping the alias to the display name. The display name should be a translation key suitable
+    for looking up multilingual translations of the display name for that facet.
+
+    :param filters_config: The application configuration
+    :return: A dictionary of values mapping the alias to the display name.
+    """
     return {f"{cfg['alias']}": f"{cfg['display_name']}" for cfg in filters_config.values()}
 
 
 def display_value_alias_map(filters_config: Dict) -> Dict:
+    """
+    A dictionary mapping the alias to any display values. This is used for mapping facet values to human-friendly values,
+    e.g., "true" and "false" might be mapped to "Yes" or "No". The values are set in the configuration file; this
+    simply provides a lookup so that we can map the incoming field alias to any potential values.
+    :param filters_config: The application configuration
+    :return: A dictionary of values mapping the alias to a set of facet value aliases.
+    """
     return {f"{cfg['alias']}": cfg['display_value_map'] for cfg in filters_config.values() if cfg.get('display_value_map')}
 
 
@@ -102,14 +129,13 @@ class SearchRequest:
         """
         Turns the incoming 'mode' request to a string suitable for use in a Solr `fq` query.
 
-        :return: A string with the appropriate "OR" statements applied.
+        :return: The alias mapping e.g., 'mode=people' to "type:person"
         """
         modes: Dict = self._app_config["search"]["modes"]
         mode_cfg: Dict = modes[self._requested_mode]
-        record_types: List = mode_cfg["record_types"]
-        solr_types_field: List = [f"type:{v}" for v in record_types]
+        record_type: str = mode_cfg["record_type"]
 
-        return " OR ".join(solr_types_field)
+        return f"type:{record_type}"
 
     def _compile_filters(self) -> List:
         filter_statements: List = []
@@ -135,20 +161,32 @@ class SearchRequest:
     def _get_facets(self) -> str:
         # if there are no filter, return an empty string. This will cause Solr to not emit any facets through
         # the JSON Facet API
-        if not self._filters_for_mode:
-            return ""
-
         json_facets: Dict = {}
+        # a list of all possible modes configured.
+        all_modes: str = " OR ".join([f"type:{v['record_type']}" for k, v in self._app_config['search']['modes'].items()])
 
-        for solr_field, field_cfg in self._filters_for_mode.items():
-            field_alias = field_cfg["alias"]
+        if self._filters_for_mode:
+            for solr_field, field_cfg in self._filters_for_mode.items():
+                field_alias = field_cfg["alias"]
 
-            json_facets[field_alias] = {
-                "type": "terms",
-                "field": solr_field,
-                "sort": f"{field_cfg['sort']}",
-                "limit": 50
+                json_facets[field_alias] = {
+                    "type": "terms",
+                    "field": solr_field,
+                    "sort": f"{field_cfg['sort']}",
+                    "limit": 50
+                }
+
+        # Add a facet for the 'mode' block. This is special since we always want to return the counts for the query
+        # regardless of the current filter selected. So if the user selects the 'person' mode, we still want to
+        # show the count for the number of sources. This also allows us to omit a mode if there are zero results.
+        json_facets["mode"] = {
+            "type": "terms",
+            "field": "type",
+            "domain": {
+                "excludeTags": "mode",
+                "filter": all_modes
             }
+        }
 
         # Serialize the JSON to a string so that it can go over the Solr API.
         # PySolr will transparently 'do the right thing' with the request.
@@ -162,7 +200,9 @@ class SearchRequest:
             use as a Solr query.
         """
         mode_filter: str = self._modes_to_filter()
-        self.filters.append(mode_filter)
+        # The tag allows us to reference this in the facets so that we can return all the types of results.
+        # See https://solr.apache.org/guide/8_8/faceting.html#tagging-and-excluding-filters
+        self.filters.append(f"{{!tag=mode}}{mode_filter}")
 
         requested_filters: List = self._compile_filters()
         self.filters += requested_filters
