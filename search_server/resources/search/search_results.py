917758767deffbb826e64079a548dd6c4b352bc8
Andrew Hankinson <andrew.hankinson@rism.digital>
2021-06-02 17:30:27 +0200

    fixed: re-factor search result handling

--- search_server/resources/search/search_results.py
+++ search_server/resources/search/search_results.py
@@ -6,6 +6,7 @@ import pysolr
 import serpy
 
 from search_server.helpers.display_fields import get_display_fields, LabelConfig
+from search_server.helpers.fields import StaticField
 from search_server.helpers.identifiers import (
     get_identifier,
     ID_SUB
@@ -23,94 +24,89 @@ class SearchResults(BaseSearchResults):
         if obj.hits == 0:
             return None
 
-        return SearchResult(obj.docs, many=True, context={"request": self.context.get("request")}).data
+        results: List[Dict] = []
+        req = self.context.get('request')
+
+        for d in obj.docs:
+            if d['type'] == "source":
+                results.append(SourceSearchResult(d, context={"request": req}).data)
+            elif d['type'] == "person":
+                results.append(PersonSearchResult(d, context={"request": req}).data)
+            elif d['type'] == "institution":
+                results.append(InstitutionSearchResult(d, context={"request": req}).data)
+            elif d['type'] == 'place':
+                results.append(PlaceSearchResult(d, context={"request": req}).data)
+            elif d['type'] == "liturgical_festival":
+                results.append(LiturgicalFestivalSearchResult(d, context={"request": req}).data)
+            elif d['type'] == "incipit":
+                results.append(IncipitSearchResult(d, context={"request": req}).data)
+            else:
+                return None
+
+        return results
 
 
-class SearchResult(ContextDictSerializer):
+class SourceSearchResult(ContextDictSerializer):
     srid = serpy.MethodField(
         label="id"
     )
     label = serpy.MethodField()
+    result_type = StaticField(
+        label="type",
+        value="rism:Source"
+    )
     type_label = serpy.MethodField(
         label="typeLabel"
-    )
-    result_type = serpy.MethodField(
-        label="type"
     )
     summary = serpy.MethodField()
     part_of = serpy.MethodField(
         label="partOf"
     )
+    flags = serpy.MethodField()
 
     def get_srid(self, obj: Dict) -> str:
         req = self.context.get('request')
         id_value: str = re.sub(ID_SUB, "", obj.get("id"))
 
-        kwargs: Dict = {}
-        route: str = ""
+        return get_identifier(req, "sources.source", source_id=id_value)
 
-        if obj["type"] == "source":
-            kwargs = {"source_id": id_value}
-            route = "sources.source"
-        elif obj["type"] == "person":
-            kwargs = {"person_id": id_value}
-            route = "people.person"
-        elif obj["type"] == "institution":
-            kwargs = {"institution_id": id_value}
-            route = "institutions.institution"
-        elif obj["type"] == "place":
-            kwargs = {"place_id": id_value}
-            route = "places.place"
-        elif obj["type"] == "liturgical_festival":
-            kwargs = {"festival_id": id_value}
-            route = "festivals.festival"
-        elif obj["type"] == "incipit":
-            # TODO: Process incipit for source id and incipit id
-            kwargs = {"incipit_id": id_value}
-            route = "incipits.incipit"
-
-        return get_identifier(req, route, **kwargs)
-
-    def get_label(self, obj: SolrResult) -> Dict:
-        label: str
-
-        if obj["type"] == "source":
-            label = obj.get("main_title_s")
-        elif obj["type"] == "institution":
-            label = _format_institution_label(obj)
-        elif obj["type"] in ("person", "institution", "liturgical_festival"):
-            label = obj.get("name_s")
-        else:
-            label = "[ Test Title ]"
+    def get_label(self, obj: Dict) -> Dict:
+        label: str = obj.get("main_title_s")
 
         return {"none": [label]}
 
-    def get_result_type(self, obj: Dict) -> str:
-        return f"rism:{obj.get('type').title()}"
+    def get_type_label(self, obj: Dict) -> Dict:
+        req = self.context.get("request")
+        transl = req.app.ctx.translations
+        return transl.get("records.source")
+
+    def get_summary(self, obj: Dict) -> Optional[List[Dict]]:
+        field_config: LabelConfig = {
+            "creator_name_s": ("records.composer_author", None),
+            "source_type_sm": ("records.source_type", None),  # TODO: The value of this field should be translatable
+        }
+
+        req = self.context.get("request")
+        transl: Dict = req.app.ctx.translations
+
+        return get_display_fields(obj, transl, field_config=field_config)
 
     def get_part_of(self, obj: SolrResult) -> Optional[Dict]:
         """
             Provides a pointer back to a parent. Used for Items in Sources and Incipits.
         """
-        obj_type: str = obj["type"]
-
-        if obj_type not in ("source", "incipit"):
+        is_item: bool = obj.get('is_item_record_b', False)
+        # if it isn't an item record, then it isn't part of anything!
+        if not is_item:
             return None
 
-        is_item: bool = obj.get('is_item_record_b', False)
         req = self.context.get("request")
 
         parent_title: str
         parent_source_id: str
 
-        if obj_type == "source" and is_item:
-            parent_title = obj.get("source_membership_title_s")
-            parent_source_id = re.sub(ID_SUB, "", obj.get("source_membership_id"))
-        elif obj_type == "incipit":
-            parent_title = obj.get("source_title_s")
-            parent_source_id = re.sub(ID_SUB, "", obj.get("source_id"))
-        else:
-            return None
+        parent_title = obj.get("source_membership_title_s")
+        parent_source_id = re.sub(ID_SUB, "", obj.get("source_membership_id"))
 
         transl: Dict = req.app.ctx.translations
 
@@ -125,48 +121,64 @@ class SearchResult(ContextDictSerializer):
             }
         }
 
+    def get_flags(self, obj: Dict) -> Optional[Dict]:
+        has_digitization: bool = obj.get("has_digitization_b", False)
+        is_item: bool = obj.get("is_item_record_b", False)
+        has_incipits: bool = obj.get("has_incipits_b", False)
+        has_iiif: bool = obj.get("has_iiif_manifest_b", False)
+        flags: Dict = {}
+
+        if has_digitization:
+            flags.update({"hasDigitization": has_digitization})
+
+        if is_item:
+            flags.update({"isItem": is_item})
+
+        if has_incipits:
+            flags.update({"hasIncipits": has_incipits})
+
+        if has_iiif:
+            flags.update({"hasIIIFManifest": has_iiif})
+
+        # return None if flags are empty.
+        return flags or None
+
+
+class PersonSearchResult(ContextDictSerializer):
+    srid = serpy.MethodField(
+        label="id"
+    )
+    label = serpy.MethodField()
+    result_type = StaticField(
+        label="type",
+        value="rism:Person"
+    )
+    type_label = serpy.MethodField(
+        label="typeLabel"
+    )
+    summary = serpy.MethodField()
+
+    def get_srid(self, obj: Dict) -> str:
+        req = self.context.get('request')
+        id_value: str = re.sub(ID_SUB, "", obj.get("id"))
+
+        return get_identifier(req, "people.person", person_id=id_value)
+
+    def get_label(self, obj: Dict) -> Dict:
+        label: str = _format_person_label(obj)
+
+        return {"none": [label]}
+
     def get_type_label(self, obj: Dict) -> Dict:
         req = self.context.get("request")
         transl = req.app.ctx.translations
-        label: Dict
 
-        if obj["type"] == "source":
-            label = transl.get("records.source")
-        elif obj["type"] == "person":
-            label = transl.get("records.person")
-        elif obj["type"] == "institution":
-            label = transl.get("records.institution")
-        elif obj["type"] == "place":
-            label = transl.get("records.place")
-        elif obj["type"] == "incipit":
-            label = transl.get("records.incipit")
-        elif obj["type"] == "liturgical_festival":
-            label = transl.get("records.liturgical_festival")
-        else:
-            label = {}
-            log.debug(obj["type"])
-
-        return label
+        return transl.get("records.person")
 
     def get_summary(self, obj: Dict) -> Optional[List[Dict]]:
-        field_config: LabelConfig
-        obj_type: str = obj['type']
-
-        if obj_type == "source":
-            field_config = {
-                "creator_name_s": ("records.composer_author", None),
-                "source_type_sm": ("records.source_type", None),  # TODO: The value of this field should be translatable
-            }
-        elif obj_type == "person":
-            field_config = {
-                "roles_sm": ("records.profession_or_function", None)
-            }
-        elif obj_type == "institution":
-            field_config = {}
-        elif obj_type == "place":
-            field_config = {}
-        else:
-            return None
+        field_config = {
+            "roles_sm": ("records.profession_or_function", None)
+        }
 
         req = self.context.get("request")
         transl: Dict = req.app.ctx.translations
@@ -174,7 +186,157 @@ class SearchResult(ContextDictSerializer):
         return get_display_fields(obj, transl, field_config=field_config)
 
 
-def _format_institution_label(obj: SolrResult) -> str:
+class InstitutionSearchResult(ContextDictSerializer):
+    srid = serpy.MethodField(
+        label="id"
+    )
+    label = serpy.MethodField()
+    result_type = StaticField(
+        label="type",
+        value="rism:Institution"
+    )
+    type_label = serpy.MethodField(
+        label="typeLabel"
+    )
+
+    def get_srid(self, obj: Dict) -> str:
+        req = self.context.get('request')
+        id_value: str = re.sub(ID_SUB, "", obj.get("id"))
+
+        return get_identifier(req, "institutions.institution", institution_id=id_value)
+
+    def get_label(self, obj: Dict) -> Dict:
+        label = _format_institution_label(obj)
+
+        return {"none": [label]}
+
+    def get_type_label(self, obj: Dict) -> Dict:
+        req = self.context.get("request")
+        transl = req.app.ctx.translations
+
+        return transl.get("records.institution")
+
+
+class PlaceSearchResult(ContextDictSerializer):
+    srid = serpy.MethodField(
+        label="id"
+    )
+    label = serpy.MethodField()
+    result_type = StaticField(
+        label="type",
+        value="rism:Place"
+    )
+    type_label = serpy.MethodField(
+        label="typeLabel"
+    )
+
+    def get_srid(self, obj: Dict) -> str:
+        req = self.context.get('request')
+        id_value: str = re.sub(ID_SUB, "", obj.get("id"))
+
+        return get_identifier(req, "places.place", place_id=id_value)
+
+    def get_label(self, obj: Dict) -> Dict:
+        label: str = obj.get("FIXME")
+
+        return {"none": [label]}
+
+    def get_type_label(self, obj: Dict) -> Dict:
+        req = self.context.get("request")
+        transl = req.app.ctx.translations
+
+        return transl.get("records.place")
+
+
+class LiturgicalFestivalSearchResult(ContextDictSerializer):
+    srid = serpy.MethodField(
+        label="id"
+    )
+    label = serpy.MethodField()
+    result_type = StaticField(
+        label="type",
+        value="rism:LiturgicalFestival"
+    )
+    type_label = serpy.MethodField(
+        label="typeLabel"
+    )
+
+    def get_srid(self, obj: Dict) -> str:
+        req = self.context.get('request')
+        id_value: str = re.sub(ID_SUB, "", obj.get("id"))
+
+        return get_identifier(req, "festivals.festival", festival_id=id_value)
+
+    def get_label(self, obj: Dict) -> Dict:
+        label: str = obj.get("name_s")
+
+        return {"none": [label]}
+
+    def get_type_label(self, obj: Dict) -> Dict:
+        req = self.context.get("request")
+        transl = req.app.ctx.translations
+
+        return transl.get("records.liturgical_festival")
+
+
+class IncipitSearchResult(ContextDictSerializer):
+    srid = serpy.MethodField(
+        label="id"
+    )
+    label = serpy.MethodField()
+    result_type = StaticField(
+        label="type",
+        value="rism:Incipit"
+    )
+    type_label = serpy.MethodField(
+        label="typeLabel"
+    )
+    part_of = serpy.MethodField(
+        label="partOf"
+    )
+
+    def get_srid(self, obj: Dict) -> str:
+        req = self.context.get('request')
+        id_value: str = re.sub(ID_SUB, "", obj.get("id"))
+
+        return get_identifier(req, "incipits.incipit", incipit_id=id_value)
+
+    def get_label(self, obj: Dict) -> Dict:
+        label: str = _format_incipit_label(obj)
+
+        return {"none": [label]}
+
+    def get_type_label(self, obj: Dict) -> Dict:
+        req = self.context.get("request")
+        transl = req.app.ctx.translations
+
+        return transl.get("records.incipit")
+
+    def get_part_of(self, obj: SolrResult) -> Optional[Dict]:
+        """
+            Provides a pointer back to the parent for this incipit
+        """
+        req = self.context.get("request")
+        parent_title: str
+        parent_source_id: str
+
+        parent_title: str = obj.get("source_title_s")
+        parent_source_id: str = re.sub(ID_SUB, "", obj.get("source_id"))
+        transl: Dict = req.app.ctx.translations
+
+        return {
+            "label": transl.get("records.item_part_of"),
+            "type": "rism:PartOfSection",
+            "source": {
+                "id": get_identifier(req, "sources.source", source_id=parent_source_id),
+                "type": "rism:Source",
+                "typeLabel": transl.get("records.source"),
+                "label": {"none": [parent_title]}
+            }
+        }
+
+
+def _format_institution_label(obj: Dict) -> str:
     city = siglum = ""
 
     if 'city_s' in obj:
@@ -183,3 +345,31 @@ def _format_institution_label(obj: SolrResult) -> str:
         siglum = f" ({obj['siglum_s']})"
 
     return f"{obj['name_s']}{city}{siglum}"
+
+
+def _format_person_label(obj: Dict) -> str:
+    name: str = obj.get("name_s")
+    dates: str = f" ({d})" if (d := obj.get("date_statement_s")) else ""
+
+    return f"{name}{dates}"
+
+
+def _format_incipit_label(obj: Dict) -> str:
+    """
+    The format for incipit titles is:
+
+    Source title: Work num (supplied title)
+
+    e.g., "Overtures - winds, stck: 1.1.1 (Allegro)"
+
+    If the supplied title is not on the record, it will be omitted.
+
+    :param obj: A Solr result object containing an incipit record
+    :return: A string of the composite title
+    """
+    work_num: str = obj['work_num_s']
+    source_title: str = obj["source_title_s"]
+    title: str = f" ({d})" if (d := obj.get("title_s")) else ""
+
+    return f"{source_title}: {work_num}{title}"
+
