05525771c60fb92e2a636fcf001ad4069bc517d1
Andrew Hankinson <andrew.hankinson@rism.digital>
2021-06-24 15:34:09 +0200

    wip: search request parser
    Changed to work with the JSON Request API
    Working on filter and facet configurations

--- search_server/helpers/search_request.py
+++ search_server/helpers/search_request.py
@@ -1,17 +1,21 @@
+import logging
+import urllib.parse
 from typing import Dict, Optional, List
 
 from search_server.exceptions import InvalidQueryException, PaginationParseException
 from search_server.resources.search.pagination import parse_page_number, parse_row_number
-import urllib.parse
-import logging
-
-import ujson
 
 log = logging.getLogger(__name__)
 
+# Some of the facets and filters need to have a solr `{!tag}` prepended. We'll
+# define them up-front.
+RANGE_FILTER_TAG: str = "RANGE_FILTER"
+SELECTOR_FILTER_TAG: str = "SELECTOR_FILTER"
+MODE_FILTER_TAG: str = "MODE_FILTER"
+
 
 # Some helper functions to work with the field and filter configurations.
-def filters_for_mode(cfg: Dict, mode: str) -> Dict:
+def filters_for_mode(cfg: Dict, mode: str) -> List:
     """
     Returns a set of facets to apply when a particular result mode is chosen.
     :param cfg: The application configuration
@@ -20,40 +24,35 @@ def filters_for_mode(cfg: Dict, mode: str) -> Dict:
         are configured.
     """
     filter_configuration: Dict = cfg["search"]["filters"]
-    filts: Optional[Dict] = filter_configuration.get(mode)
-    return filts or {}
+    filts: List = filter_configuration.get(mode, [])
+    return filts
 
 
-def field_alias_map(filters_config: Dict) -> Dict:
+def filter_type_map(filters_config: List) -> Dict:
+    """
+    A dictionary that maps the type of filter (toggle, selector, etc.) to the alias.
+    :param filters_config:
+    :return:
+    """
+    return {f"{cnf['alias']}": f"{cnf['type']}" for cnf in filters_config}
+
+
+def filter_label_map(filters_config: List) -> Dict:
+    return {f"{cnf['alias']}": f"{cnf['label']}" for cnf in filters_config}
+
+
+def alias_config_map(filters_config: List) -> Dict:
+    return {f"{cnf['alias']}": cnf for cnf in filters_config}
+
+
+def field_alias_map(filters_config: List) -> Dict:
     """
     A dictionary mapping the configured alias to the Solr field. Used for referencing the solr field using the
     alias.
     :param filters_config: The application configuration
     :return: A dictionary of values mapping the alias to the solr field.
     """
-    return {f"{cnf['alias']}": f"{solr_f}" for solr_f, cnf in filters_config.items()}
-
-
-def display_name_alias_map(filters_config: Dict) -> Dict:
-    """
-    A dictionary mapping the alias to the display name. The display name should be a translation key suitable
-    for looking up multilingual translations of the display name for that facet.
-
-    :param filters_config: The application configuration
-    :return: A dictionary of values mapping the alias to the display name.
-    """
-    return {f"{cfg['alias']}": f"{cfg['display_name']}" for cfg in filters_config.values()}
-
-
-def display_value_alias_map(filters_config: Dict) -> Dict:
-    """
-    A dictionary mapping the alias to any display values. This is used for mapping facet values to human-friendly values,
-    e.g., "true" and "false" might be mapped to "Yes" or "No". The values are set in the configuration file; this
-    simply provides a lookup so that we can map the incoming field alias to any potential values.
-    :param filters_config: The application configuration
-    :return: A dictionary of values mapping the alias to a set of facet value aliases.
-    """
-    return {f"{cfg['alias']}": cfg['display_value_map'] for cfg in filters_config.values() if cfg.get('display_value_map')}
+    return {f"{cnf['alias']}": f"{cnf['field']}" for cnf in filters_config}
 
 
 class SearchRequest:
@@ -66,6 +65,13 @@ class SearchRequest:
     While it's primary function is to interact with Solr for the main search interface, it can also be used
     in other places where paginated interactions with Solr are required, such as viewing a person's list of
     related sources.
+
+    A bit of terminology to be aware of:
+        - FILTERS are requests sent to Solr to filter queries
+        - FACETS are responses from Solr giving us the values for specific facet fields
+
+    In other words: Values from FACETS are then used by the client to compose requests for FILTERS.
+
     """
     default_sort = "id asc"
 
@@ -88,8 +94,11 @@ class SearchRequest:
         self._page: Optional[str] = req.args.get("page", None)
         self._return_rows: Optional[str] = req.args.get("rows", None)
 
-        self._filters_for_mode: Dict = filters_for_mode(self._app_config, self._requested_mode)
-        self._alias_map: Dict = field_alias_map(self._filters_for_mode)
+        # Configure the facets to show for the selected mode.
+        self._facets_for_mode: List = filters_for_mode(self._app_config, self._requested_mode)
+        self._alias_map: Dict = field_alias_map(self._facets_for_mode)
+        self._alias_config_map: Dict = alias_config_map(self._facets_for_mode)
+        self._filter_type_map: Dict = filter_type_map(self._facets_for_mode)
 
     def _validate_incoming_request(self) -> None:
         """
@@ -148,33 +157,59 @@ class SearchRequest:
             unencoded_value: str = urllib.parse.unquote_plus(raw_value)
 
             # Then remove any quotes (single or double)
-            value: str = unencoded_value.replace("\"", "").replace("'", "")
+            quoted_value: str = unencoded_value.replace("\"", "").replace("'", "")
 
-            # Finally, ensure that we *always* pass it to Solr as a quoted value. (The single quotes here will
-            # get converted to double-quotes by the internals of the pysolr API). We use the previously-constructed
-            # alias map to map the public field name to the private solr field.
-            new_val: str = f"{self._alias_map[field]}:\"{value}\""
+            # Finally, ensure that we pass it to Solr as a quoted value if it is not a range search.
+            # This is because Solr interprets spaces in an unquoted value to be an implicit "AND".
+            # Range queries will not work when they are quoted, however.
+            # We use the previously-constructed alias map to map the public field name to the
+            # private solr field.
+            value: str
+
+            if quoted_value.startswith("["):
+                value = quoted_value
+            else:
+                value = f"\"{quoted_value}\""
+
+            new_val: str = f"{self._alias_map[field]}:{value}"
+
+            # Some field types need to be tagged to help modify their behaviour and interactions with
+            # facets for multi-select faceting. See:
+            # https://solr.apache.org/guide/8_8/faceting.html#tagging-and-excluding-filters
+            field_type: str = self._filter_type_map[field]
+            if field_type == "range":
+                new_val = f"{{!tag={RANGE_FILTER_TAG}}}{new_val}"
+            elif field_type == "selector":
+                new_val = f"{{!tag={SELECTOR_FILTER_TAG}}}{new_val}"
+
             filter_statements.append(new_val)
 
         return filter_statements
 
-    def _get_facets(self) -> str:
+    def _get_facets(self) -> dict:
         # if there are no filter, return an empty string. This will cause Solr to not emit any facets through
         # the JSON Facet API
-        json_facets: Dict = {}
+        json_facets: dict = {}
         # a list of all possible modes configured.
         all_modes: str = " OR ".join([f"type:{v['record_type']}" for k, v in self._app_config['search']['modes'].items()])
 
-        if self._filters_for_mode:
-            for solr_field, field_cfg in self._filters_for_mode.items():
-                field_alias = field_cfg["alias"]
+        if self._facets_for_mode:
+            for facet_cfg in self._facets_for_mode:
+                solr_facet_def: dict
+                facet_alias = facet_cfg["alias"]
 
-                json_facets[field_alias] = {
-                    "type": "terms",
-                    "field": solr_field,
-                    "sort": f"{field_cfg['sort']}",
-                    "limit": 50
-                }
+                if facet_cfg['type'] == "range":
+                    solr_facet_def = _create_range_facet(facet_cfg)
+                elif facet_cfg['type'] == "toggle":
+                    solr_facet_def = _create_toggle_facet(facet_cfg)
+                elif facet_cfg['type'] == "selector":
+                    solr_facet_def = _create_selector_facet(facet_cfg)
+                elif facet_cfg["type"] == "filter":
+                    solr_facet_def = _create_filter_facet(facet_cfg)
+                else:
+                    continue
+
+                json_facets[facet_alias] = solr_facet_def
 
         # Add a facet for the 'mode' block. This is special since we always want to return the counts for the query
         # regardless of the current filter selected. So if the user selects the 'person' mode, we still want to
@@ -183,14 +218,15 @@ class SearchRequest:
             "type": "terms",
             "field": "type",
             "domain": {
-                "excludeTags": "mode",
+                "excludeTags": MODE_FILTER_TAG,
                 "filter": all_modes
             }
         }
 
-        # Serialize the JSON to a string so that it can go over the Solr API.
-        # PySolr will transparently 'do the right thing' with the request.
-        return ujson.dumps(json_facets)
+        return json_facets
+
+    def _compile_sorts(self) -> str:
+        return ",".join(self.sorts)
 
     def compile(self) -> Dict:
         """
@@ -202,7 +238,7 @@ class SearchRequest:
         mode_filter: str = self._modes_to_filter()
         # The tag allows us to reference this in the facets so that we can return all the types of results.
         # See https://solr.apache.org/guide/8_8/faceting.html#tagging-and-excluding-filters
-        self.filters.append(f"{{!tag=mode}}{mode_filter}")
+        self.filters.append(f"{{!tag={MODE_FILTER_TAG}}}{mode_filter}")
 
         requested_filters: List = self._compile_filters()
         self.filters += requested_filters
@@ -213,10 +249,88 @@ class SearchRequest:
         start_row: int = 0 if page_num == 1 else ((page_num - 1) * return_rows)
 
         return {
-            "q": [self._requested_query],
-            "fq": self.filters,
-            "start": start_row,
-            "rows": return_rows,
-            # "sort": ", ".join(sorts),
-            "json.facet": self._get_facets()
+            "query": self._requested_query,
+            "filter": self.filters,
+            "offset": start_row,
+            "limit": return_rows,
+            "sort": self._compile_sorts(),
+            "facet": self._get_facets()
         }
+
+
+def _create_range_facet(facet_cfg: Dict) -> Dict:
+    """
+    Creates a JSON facet API configuration that will return the min
+    and max values of a scalar field (e.g., integers, dates, etc.)
+
+    This range facet will EXCLUDE any filters tagged with the {!tag=RANGE_FILT}.
+    This makes it possible to do a query with a bunch of other facets and filters,
+    but to continue showing the maximum range possible for the specific field. This
+    allows users to continue to move the "start" and "end" slider through the whole
+    range of values, instead of being constrained by the max and min of the currently
+    applied filters.
+
+    This is configured as a 'query' facet because Solr is a bit dumb and won't otherwise
+    let you create dedicated stats blocks. See:
+
+    https://stackoverflow.com/questions/46450477/in-addition-to-the-query-retrieve-min-and-max-of-a-field-with-local-paramete
+
+    :param facet_cfg: A facet configuration block from the config file.
+    :return: A JSON Facet API configuration block.
+    """
+    field_name: str = facet_cfg["field"]
+
+    cfg: Dict = {
+        "type": "query",
+        "q": "*:*",
+        "facet": {
+            "min": f"min({field_name})",
+            "max": f"max({field_name})"
+        },
+        "domain": {
+            "excludeTags": [RANGE_FILTER_TAG]
+        }
+    }
+    return cfg
+
+
+def _create_toggle_facet(facet_cfg: Dict) -> Dict:
+    field_name: str = facet_cfg["field"]
+
+    cfg: Dict = {
+        "type": "terms",
+        "field": f"{field_name}",
+        "limit": 2
+    }
+
+    return cfg
+
+
+def _create_selector_facet(facet_cfg: Dict) -> Dict:
+    field_name: str = facet_cfg["field"]
+
+    cfg: Dict = {
+        "type": "terms",
+        "field": f"{field_name}",
+        "domain": {
+            "excludeTags": [SELECTOR_FILTER_TAG]
+        }
+    }
+
+    if sort := facet_cfg.get("sort"):
+        cfg.update({"sort": sort})
+
+    return cfg
+
+
+def _create_filter_facet(facet_cfg: Dict) -> Dict:
+    field_name: str = facet_cfg["field"]
+    cfg: Dict = {
+        "type": "terms",
+        "field": f"{field_name}",
+    }
+
+    if sort := facet_cfg.get("sort"):
+        cfg.update({"sort": sort})
+
+    return cfg
