827331318f537ef019e0c1a5b60c8643be916693
Andrew Hankinson <andrew.hankinson@rism.digital>
2021-08-23 15:57:20 +0200

    new: incipit rendering service
    Passing plaine and easie to this service will return some SVG. Will be used for rendering notation in response to some input.

---
+++ search_server/resources/incipits/render.py
@@ -0,0 +1,44 @@
+from typing import Optional
+
+from sanic import response
+
+from search_server.helpers.vrv import create_pae_from_request, render_pae, RenderedPAE
+
+
+async def handle_incipit_render(req) -> response.HTTPResponse:
+    """ Takes an incoming string and runs it through Verovio to render as notation. Requests use the `?n=` query
+        parameter as required notation input.
+
+        Other optional parameters that may be used include:
+         - ic: clef (default: G-2)
+         - it: time signature (default: 4/4)
+         - ik: key signature (default: no sharps, no flats -- key of C major)
+
+        (This is the same as the request parameters for the incipit search)
+
+        Returns SVG in the body of the response
+    """
+    requested_notation: Optional[str] = req.args.get("n")
+
+    if not requested_notation:
+        return response.text(
+            "No notation input supplied",
+            status=400
+        )
+
+    pae: str = create_pae_from_request(req, requested_notation)
+
+    rendered_pae: Optional[RenderedPAE] = render_pae(pae)
+    if not rendered_pae:
+        return response.text(
+            "There was a problem rendering the Plaine and Easie notation",
+            status=500
+        )
+
+    svg, _ = rendered_pae
+    response_headers = {'Content-Type':  "image/svg+xml;charset=utf8"}
+
+    return response.text(
+        svg,
+        headers=response_headers
+    )
--- search_server/routes/incipits.py
+++ search_server/routes/incipits.py
@@ -1,8 +1,15 @@
 from sanic import Blueprint
 
+from search_server.resources.incipits.render import handle_incipit_render
+
 incipits_blueprint: Blueprint = Blueprint("incipits", url_prefix="/incipits")
 
 
 @incipits_blueprint.route("/<incipit_id:string>")
 async def incipit(req, incipit_id: str):
     pass
+
+
+@incipits_blueprint.route("/render")
+async def incipit_render(req):
+    return await handle_incipit_render(req)
