b77295d527a6a56eb459797d6194a3505906ad38
Andrew Hankinson <andrew.hankinson@gmail.com>
2022-06-14 18:21:51 +0200

    Fixed: Add source contents search to API
    Fixes rism-digital/rism-online-issues#61

--- search_server/routes/sources.py
+++ search_server/routes/sources.py
@@ -1,7 +1,9 @@
 from sanic import Blueprint
 
-from search_server.request_handlers import handle_request
+from search_server.request_handlers import handle_request, handle_search
 from search_server.resources.incipits.incipit import handle_incipits_list_request, handle_incipit_request
+from search_server.resources.sources.contents_search import handle_contents_search_request, \
+    handle_contents_probe_request
 from search_server.resources.sources.handlers import (
     handle_source_request,
 )
@@ -32,13 +34,24 @@ async def incipit(req, source_id: str, work_num: str):
                                 work_num=work_num)
 
 
-@sources_blueprint.route("/<source_id:str>/relationships/")
-async def relationships(req, source_id: str):
-    pass
-
-
 @sources_blueprint.route("/<source_id:str>/contents/")
 async def contents(req, source_id: str):
+    return await handle_search(req,
+                               handle_contents_search_request,
+                               source_id=source_id)
+
+
+@sources_blueprint.route("/<source_id:str>/probe/")
+async def probe(req, source_id: str):
+    return await handle_search(req,
+                               handle_contents_probe_request,
+                               source_id=source_id)
+
+
+
+
+@sources_blueprint.route("/<source_id:str>/relationships/")
+async def relationships(req, source_id: str):
     pass
 
 
