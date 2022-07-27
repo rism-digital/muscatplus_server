69db9ce123edbfdba95e53e5f725e7e8ca552892
Andrew Hankinson <andrew.hankinson@gmail.com>
2022-07-27 15:46:42 +0200

    Fixed: Opengraph response label

--- data_export_server/resources/opengraph.py
+++ data_export_server/resources/opengraph.py
@@ -107,7 +107,19 @@ class OpenGraphSvg(ContextDictSerializer):
         title: str
 
         if obj["type"] == "source":
-            title = format_source_label(obj)
+            main_title: str = obj.get("main_title_s", "[No title]")
+            #  TODO: Translate source types
+            source_types: Optional[list] = obj.get("material_group_types_sm")
+            shelfmark: Optional[str] = obj.get("shelfmark_s")
+            siglum: Optional[str] = obj.get("siglum_s")
+
+            label: str = main_title
+            if source_types:
+                label = f"{label}; {', '.join(source_types)}"
+            if siglum and shelfmark:
+                label = f"{label}; {siglum} {shelfmark}"
+
+            title = label
         elif obj["type"] == "person":
             title = format_person_label(obj)
         elif obj["type"] == "institution":
