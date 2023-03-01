2e36205e3e5872c0d4261f359e4bed6e1944f426
Andrew Hankinson <andrew.hankinson@gmail.com>
2023-03-01 16:05:15 +0100

    Some enhancements for the export script
    Does less per document loop.

--- linked_data/export.py
+++ linked_data/export.py
@@ -1,10 +1,12 @@
 import argparse
 import asyncio
+import concurrent.futures
 import logging
 import logging.config
 import shutil
 import timeit
 from pathlib import Path
+from typing import Iterable, Callable
 
 import aiofiles
 import rdflib
@@ -19,7 +21,7 @@ from search_server.resources.institutions.institution import Institution
 from search_server.resources.people.person import Person
 from search_server.resources.sources.full_source import FullSource
 from search_server.server import app
-from shared_helpers.jsonld import RISM_JSONLD_SOURCE_CONTEXT
+from shared_helpers.jsonld import RISM_JSONLD_SOURCE_CONTEXT, RISM_JSONLD_PERSON_CONTEXT, RISM_JSONLD_DEFAULT_CONTEXT
 from shared_helpers.languages import load_translations, filter_languages
 
 # Prevent other loggers from logging
@@ -58,6 +60,15 @@ async def main(args: argparse.Namespace) -> bool:
     translations: dict = load_translations("locales/")
     filt_translations: dict = filter_languages({"en"}, translations)
 
+    route = Route()
+
+    # Fake a sanic request object. We patch a bunch of things to the request to add exactly what the request
+    # needs to serialize the output. This includes the URL, of which the path part gets ignored -- the important
+    # bit is in the headers!
+    req = Request(bytes("/foo", "ascii"), headers, "", "GET", TransportProtocol(), app)
+    req.ctx.translations = filt_translations
+    req.route = route
+
     if args.empty:
         log.info("Emptying output directory %s", args.output)
         shutil.rmtree(args.output)
@@ -73,56 +84,47 @@ async def main(args: argparse.Namespace) -> bool:
     log.debug("Processing record types: %s", inc)
 
     for resource_type in inc:
-        log.debug("Processing %s", resource_type)
+        log.info("Processing %s", resource_type)
         if resource_type in args.exclude:
             continue
 
         if resource_type == "institutions":
             record_type = "institution"
+            ctx = RISM_JSONLD_DEFAULT_CONTEXT
+            serializer = Institution
         elif resource_type == "people":
             record_type = "person"
-        else:
+            ctx = RISM_JSONLD_PERSON_CONTEXT
+            serializer = Person
+        elif resource_type == "sources":
             record_type = "source"
+            ctx = RISM_JSONLD_SOURCE_CONTEXT
+            serializer = FullSource
+        else:
+            log.debug("Could not determin serializer, so we are skipping resource type %s", resource_type)
+            continue
 
         s = Solr("http://localhost:8983/solr/muscatplus_live")
         fq = [f"type:{record_type}"]
 
-        if args.country_code and resource_type in ("sources", ):
+        if args.country_code and resource_type == "sources":
             log.info("Restricting to country %s", args.country_code)
             fq.append(f"country_code_s:{args.country_code}")
+        elif args.country_code and resource_type == "institutions":
+            fq.append(f"country_codes_sm:{args.country_code}")
 
         sort = "id asc"
-        ctx_val = {"@context": RISM_JSONLD_SOURCE_CONTEXT}
+        ctx_val = {"@context": ctx}
         res = await s.search({"query": "*:*", "filter": fq, "sort": sort, "limit": 500}, cursor=True)
         num_records += res.hits
-        route = Route()
 
         async for sdoc in res:
             sid = sdoc['rism_id']
-            url: str = f"/{resource_type}/{sid}"
-            log.info("Processing %s", url)
-
-            # Fake a sanic request object. We patch a bunch of things to the request to add exactly what the request
-            # needs to serialize the output.
-            req = Request(bytes(url, "ascii"), headers, "", "GET", TransportProtocol(), app)
-            req.ctx.translations = filt_translations
-            req.route = route
-
-            if resource_type == "sources":
-                f = await FullSource(sdoc, context={"request": req,
-                                                    "direct_request": True}).data
-            elif resource_type == "people":
-                f = await Person(sdoc,  context={"request": req,
-                                                 "direct_request": True}).data
-            elif resource_type == "institutions":
-                f = await Institution(sdoc,  context={"request": req,
-                                                      "direct_request": True}).data
-            else:
-                log.error("Error with resource type %s", resource_type)
-                continue
+            serialized = await serializer(sdoc, context={"request": req,
+                                                         "direct_request": True}).data
 
             # Add the context value to the resulting dictionary so that we can serialize it
-            f.update(ctx_val)
+            serialized.update(ctx_val)
 
             # Writes the output to a balanced directory structure, so that no single directory has too many
             # files. Uses the last three digits of the
@@ -131,20 +133,22 @@ async def main(args: argparse.Namespace) -> bool:
             if not outpath.exists():
                 outpath.mkdir(parents=True)
 
-            turtle: str = await asyncio.get_running_loop().run_in_executor(None, to_turtle, f)
+            turtle: str = await asyncio.get_running_loop().run_in_executor(None, to_turtle, serialized)
 
             async with aiofiles.open(str(Path(outpath, f"{sid}.ttl")), mode="w") as ttlout:
                 await ttlout.write(turtle)
 
-            outp |= True
+        elapsed_for_resource_type: float = timeit.default_timer() - start
+        log.info(f"Processing rate for {resource_type}: {res.hits / elapsed_for_resource_type} docs/s")
+        outp |= True
 
     end = timeit.default_timer()
     elapsed: float = end - start
 
     hours, remainder = divmod(elapsed, 60 * 60)
     minutes, seconds = divmod(remainder, 60)
-    log.info(f"Total time to download: {int(hours):02}:{int(minutes):02}:{round(seconds):02} (Total: {elapsed})")
-    log.info(f"Processing rate: {num_records / elapsed} docs/s")
+    log.info(f"Total time to download: {int(hours):02}:{int(minutes):02}:{round(seconds):02} (Total: {elapsed}s)")
+    log.info(f"Total processing rate: {num_records / elapsed} docs/s")
 
     return outp
 
