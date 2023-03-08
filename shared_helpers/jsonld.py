d3cb7732f7ac2267dcca3bf5ecaf79375344d521
Andrew Hankinson <andrew.hankinson@gmail.com>
2023-03-08 18:12:26 +0100

    Update JSON-LD context documents

--- shared_helpers/jsonld.py
+++ shared_helpers/jsonld.py
@@ -18,6 +18,7 @@ __BASE_CONTEXT = {
     # "geojson": "https://purl.org/geojson/vocab#",
     "schemaorg": "https://schema.org/",
     "rdau": "http://rdaregistry.info/Elements/u/",
+    "xsd": "http://www.w3.org/2001/XMLSchema#",
     "type": "@type",
     "id": "@id",
     # "none": "@none",
@@ -58,6 +59,45 @@ __RELATIONSHIPS = {
     }
 }
 
+__INCIPITS = {
+    "incipits": {
+        "@id": "rism:hasIncipit",
+        "@type": "@id",
+        "@context": {
+            "items": "@set",
+            "properties": "@nest",
+            "clef": {
+                "@id": "rism:hasPAEClef"
+            },
+            "keysig": {
+                "@id": "rism:hasPAEKeysig"
+            },
+            "timesig": {
+                "@id": "rism:hasPAETimesig"
+            },
+            "notation": {
+                "@id": "rism:hasPAEData"
+            },
+            "encodings": {
+                "@id": "rism:hasEncoding",
+                "@type": "@set",
+                "@context": {
+                    "data": {
+                        "@id": "rism:paeEncoding",
+                        "@type": "@json"
+                    },
+                    "url": {
+                        "@id": "rism:meiEncoding",
+                        "@type": "xsd:anyURI"
+                    }
+                }
+            },
+            "partOf": {"@value": "null", "propagate": "false"}
+        },
+    }
+}
+
+
 RISM_JSONLD_DEFAULT_CONTEXT: ContextDocument = {
     **__BASE_CONTEXT
 }
@@ -69,39 +109,23 @@ RISM_JSONLD_PERSON_CONTEXT: ContextDocument = {
 }
 
 
+RISM_JSONLD_WORK_CONTEXT: ContextDocument = {
+    **__BASE_CONTEXT
+}
+
 RISM_JSONLD_SOURCE_CONTEXT: ContextDocument = {
     **__BASE_CONTEXT,
     **__RELATIONSHIPS,
-    "summary": {
-        "@type": "@id",
-        "@id": "rism:hasSummary",
-        "@container": "@set",
-        "@propagate": "false"
-
+    **__INCIPITS,
+    "dates": {
+        "@id": "rism:hasDates",
+        "@context": {
+            "earliestDate": {
+                "@id": "rism:earliestDate",
+                "@type": "xsd:integer"
+            }
+        }
     },
-    # "contents": "@nest",
-    # "contents": {
-    #     "@id": "rism:hasContents",
-    #     "@type": "@id",
-    #     "@context": {
-    #         "@propagate": "false",
-    #         "label": {"@value": "null", "@propagate": "false"},
-    #         "summary": {
-    #             "@type": "@id",
-    #             "@id": "rism:hasSummary",
-    #             "@container": "@set",
-    #             "@context": {
-    #                 "label": {
-    #                     "@id": "rdfs:label",
-    #                     "@container": [
-    #                         "@language",
-    #                         "@set"
-    #                     ]
-    #                 }
-    #             }
-    #         }
-    #     }
-    # },
     "creator": {
         "@id": "dcterms:creator",
         "@type": "@id",
@@ -114,20 +138,47 @@ RISM_JSONLD_SOURCE_CONTEXT: ContextDocument = {
         "@type": "@id",
         "@context": {
             "label": {"@value": "null", "@propagate": "false"},
+            "items": "@set",
+            "summary": {
+                "@id": "rism:hasSummary",
+                "@type": "@id",
+            },
+        },
+    },
+    "partOf": {
+        "@id": "rism:isPartOf",
+        "@type": "@id",
+        "@context": {
+            "label": {"@value": "null", "@propagate": "false"},
+            "source": "@nest"
+        }
+    },
+    "sourceItems": {
+        "@id": "rism:hasSourceItem",
+        "@type": "@id",
+        "@context": {
+            "label": {"@value": "null", "@propagate": "false"},
             "items": "@set"
         }
+    },
+    "exemplars": {
+        "@id": "rism:hasHolding",
+        "@type": "@id",
+        "@context": {
+            "label": {"@value": "null", "@propagate": "false"},
+            "items": "@set",
+            "heldBy": {
+                "@id": "rism:hasHoldingInstitution"
+            }
+        }
     }
-    # "summary": {
-    #     "@id": "rism:Summary",
-    #     "@type": "@id"
-    # },
 }
-
 
 RouteOptions = namedtuple("RouteOptions", ["route", "context"])
 
 RouteContextMap: dict[str, RouteOptions] = {
-    "mp_server.people.person": RouteOptions("person_context", RISM_JSONLD_PERSON_CONTEXT),
-    "mp_server.sources.source": RouteOptions("source_context", RISM_JSONLD_SOURCE_CONTEXT),
-    "__default": RouteOptions("default_context", RISM_JSONLD_DEFAULT_CONTEXT)
+    "mp_server.people.person": RouteOptions("api.person_context", RISM_JSONLD_PERSON_CONTEXT),
+    "mp_server.sources.source": RouteOptions("api.source_context", RISM_JSONLD_SOURCE_CONTEXT),
+    "mp_server.works.work": RouteOptions("api.work_context", RISM_JSONLD_WORK_CONTEXT),
+    "__default": RouteOptions("api.default_context", RISM_JSONLD_DEFAULT_CONTEXT)
 }
