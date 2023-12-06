7706426f43fcfdcb1f838809f29b0b65a67729d0
Andrew Hankinson <andrew.hankinson@gmail.com>
2023-12-06 13:00:09 +0100

    Fixed: Ensure sigla are percent-decoded before query
    Also adds a warning log entry if more than one result is returned for a given siglum.

--- data_export_server/routes/sigla.py
+++ data_export_server/routes/sigla.py
@@ -1,5 +1,7 @@
+import logging
 import re
 from typing import Optional
+from urllib.parse import unquote
 
 from sanic import Blueprint, response
 from small_asc.client import Results
@@ -9,15 +11,21 @@ from shared_helpers.solr_connection import SolrConnection
 
 sigla_blueprint: Blueprint = Blueprint("sigla", url_prefix="/sigla")
 
+log = logging.getLogger("mp_export")
+
 
 async def handle_institution_sigla_request(req, siglum: str) -> Optional[str]:
-    fq: list = ["type:institution", f"siglum_s:{siglum}"]
+    # ensure characters are handled as UTF-8 using the 'unquote' method.
+    fq: list = ["type:institution", f"siglum_s:{unquote(siglum)}"]
     institution_record: Results = await SolrConnection.search({"query": "*:*",
                                                                "filter": fq,
                                                                "fields": ["id"]})
 
     if institution_record.hits == 0:
         return None
+
+    if institution_record.hits > 1:
+        log.warning("More than one result was returned for siglum %s. This shouldn't happen.", siglum)
 
     institution_record_id: str = institution_record.docs[0]["id"]
     institution_id = re.sub(ID_SUB, "", institution_record_id)
