fbe3fbecce5cd0d8457236b25c406568e89293a6
Andrew Hankinson <andrew.hankinson@gmail.com>
2024-04-23 08:58:02 +0200

    Fixed: Problem with the previous field
    This should fix the problem, but I'm not sure why it failed...

--- search_server/resources/siglum/sigla.py
+++ search_server/resources/siglum/sigla.py
@@ -55,7 +55,7 @@ async def handle_siglum_search_request(req) -> Optional[dict]:
         return None
 
     query_solr_fields: dict[str, str] = {"name": "names_ft",
-                                         "siglum": "siglum_lookup_kwa",
+                                         "siglum": "siglum_s",
                                          "city": "city_ft",
                                          "country": "country_names_ft",
                                          "all": ""}
