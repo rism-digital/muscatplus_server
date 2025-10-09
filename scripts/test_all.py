6fe6a647c2e76348dbead94aa45f0b6753a1454b
Andrew Hankinson <andrew.hankinson@gmail.com>
2025-10-09 17:01:20 +0200

    Fixed URL testing script

--- scripts/test_all.py
+++ scripts/test_all.py
@@ -1,51 +1,62 @@
 import asyncio
-import re
 import timeit
 
-import aiohttp
+import httpx
 import uvloop
 from small_asc.client import Solr
 
 asyncio.set_event_loop(uvloop.new_event_loop())
 
 
-async def fetch(url: str, session, num: int) -> bool:
-    print(f"{num}. fetching {url}")
-    async with session.get(url) as r:
-        txt = await r.json()
-        # print(txt)
-        if r.status == 200:
-            return True
+async def do_something(results, session) -> tuple[int, int]:
+    succ = 0
+    fail = 0
 
-        print(f"Error fetching {url}")
-        return False
-
-
-async def do_something(results):
     for result in results:
-        pass
+        rid = result.get("id")
+        if not rid:
+            print("no id!")
+            fail += 1
+            continue
+
+        rr = session.get(rid)
+        if rr.status_code == 200:
+            succ += 1
+        else:
+            fail += 1
+
+    return succ, fail
 
 
-async def run_search() -> (int, int):
-    async with aiohttp.ClientSession(
+async def search_request(url, session, succ, fail):
+    r = await session.get(url)
+    response = r.json()
+    search_info = response.get("view")
+    this_page = search_info["thisPage"]
+    total_pages = search_info["totalPages"]
+    results = search_info["results"]
+    next = search_info["next"]
+
+    p_succ, p_fail = await do_something(results, session)
+
+    succ += p_succ
+    fail += p_fail
+
+    while this_page <= total_pages:
+        print(f"Processing page {this_page} of {total_pages}")
+        _ = await search_request(next, session, succ, fail)
+
+    return succ, fail
+
+
+async def run_search() -> tuple[int, int]:
+    async with httpx.AsyncClient(
         headers={"Accept": "application/ld+json", "X-API-Accept-Language": "en"}
-    ) as session, session.get("https://rism.online/search?mode=people") as r:
-        response = await r.json()
-        search_info = response.get("view")
-        this_page = search_info["thisPage"]
-        total_pages = search_info.get("totalPages")
-        while total_pages > 0:
-            _ = await do_something(response["items"])
-            print("finished processing page", this_page)
+    ) as session:
+        initial_url = "https://rism.online/search?mode=people"
+        succ, fail = await search_request(initial_url, session, 0, 0)
 
-            with await session.get(search_info["next"]) as r:
-                response = await r.json()
-                search_info = response.get("view")
-                this_page = search_info["thisPage"]
-
-            total_pages -= 1
-
-    return (0, 0)
+    return succ, fail
 
 
 async def get_ids():
@@ -53,7 +64,7 @@ async def get_ids():
     # fq = ["type:source", "country_code_s:CH"]
     # fq = ["type:source", "project_s:diamm"]
     # fq = ["type:institution", "project_s:diamm"]
-    fq = ["type:institution", "!project_s:diamm"]
+    fq = ["type:institution", "!project_s:diamm", "!project_s:cantus"]
     # fq = ["type:person", "project_s:diamm"]
     # fq = ["type:person", "!project_s:diamm"]
     # fq = ["type:institution"]
@@ -65,30 +76,32 @@ async def get_ids():
         {"query": "*:*", "filter": fq, "fields": fl, "sort": sort, "limit": 500},
         cursor=True,
     )
-    id_sub = re.compile(
-        r"source_|person_|institution_|diamm_source_|diamm_organization_|diamm_person_"
-    )
     print(f"Assembling {res.hits} IDs")
     ids: list = []
     async for s in res:
-        ids.append(re.sub(id_sub, "", s.get("id")))
+        ids.append(s.get("id", "").split("_")[-1])
 
     print(f"Actually got {len(ids)}")
     return ids
 
 
-async def run() -> (int, int):
+async def run() -> tuple[int, int]:
     item_ids: list = await get_ids()
     responses: list = []
 
-    async with aiohttp.ClientSession(
+    async with httpx.AsyncClient(
         headers={"Accept": "application/ld+json", "X-API-Accept-Language": "en"}
     ) as session:
         for num, itm in enumerate(item_ids):
-            url: str = f"https://rism.online/institutions/{itm}"
+            print(f"Processing record {num}")
+            url: str = f"http://dev.rism.offline/institutions/{itm}"
             # url: str = f"http://dev.rism.offline/external/diamm/person/{itm}"
-            res: bool = await fetch(url, session, num)
-            responses.append(res)
+            res = await session.get(url)
+            if res.status_code in (200, 410):
+                responses.append(True)
+            else:
+                print(f"Error fetching {url}. Response code: {res.status_code}")
+                responses.append(False)
 
     successes: int = responses.count(True)
     failures: int = responses.count(False)
@@ -98,7 +111,8 @@ async def run() -> (int, int):
 
 async def main():
     start = timeit.default_timer()
-    successes, failures = await run_search()
+    successes, failures = await run()
+    # successes, failures = await run_search()
     end = timeit.default_timer()
     elapsed: float = end - start
 
