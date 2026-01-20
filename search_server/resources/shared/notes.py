5e45e8cb2f928f4a7f01826f2071a1009a6e4bf3
Andrew Hankinson <andrew.hankinson@gmail.com>
2026-01-20 10:41:30 +0100

    New: Deliver source data found on API

--- search_server/resources/shared/notes.py
+++ search_server/resources/shared/notes.py
@@ -34,6 +34,10 @@ class NotesSection(ypres.DictSerializer):
                 "records.bibliographic_reference",
                 secondary_literature_json_value_translator,
             ),
+            "source_data_found_json": (
+                "records.source_data_found",
+                secondary_literature_json_value_translator,
+            ),
         }
 
         return get_display_fields(obj, transl, field_config)
